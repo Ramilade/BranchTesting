1701538efa570c63c3b6fde8471022f9890150c1
Ramilade <rami0904@stud.kea.dk>
2022-04-05 09:19:45 +0200

    Gjorde noget som jeg ikke ved hvad er

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,4 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+    Dog dog = new Dog("Helmute", "barf");
+    Cat cat = new Cat("Lotti", "Pudr");
 }
